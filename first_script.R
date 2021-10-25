bc50b4470b9585de5ccd940d9b594176e97c2a3d
SLStrey <SL.Strey@protonmail.com>
2021-10-25 19:10:45 +0200

    file management attempt 1

--- first_script.R
+++ first_script.R
@@ -3,3 +3,11 @@ b = 2
 c = 3
 # Let's add d now, to practice making another commit
 d = 4
+#The next thing we need to do is 
+#work out file management
+
+#first attempt
+lungcancer <- readRDS("~/R_stuff/test_repo/lungcancer.Rds")
+
+#testing
+head(lungcancer)
